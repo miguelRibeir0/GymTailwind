12080c7c52b89e716c58b04c1d999c77cae7cb15
Miguel <miguelsribeiro7@gmail.com>
2024-03-08 02:27:22 +0000

    finished landing (missing animations)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       height: {
         main: "700px",
+        largescreen: "34rem",
+        card: "27rem",
       },
       backgroundImage: {
         landing: "url('../images/landing.jpg')",
